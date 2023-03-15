e7a92a8b45e780c96418fa6bdbf1c5b449bdd5e8
Marcelo Lemos <108501838+Lemmores@users.noreply.github.com>
2023-03-15 11:21:30 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
       </Head>
       <h1 className={styles.title}>Você foi convocado para LemosParty!</h1>
       <br></br>
-       <iframe width="380" height="315" src="https://www.youtube.com/embed/kcuZuR70Lpc" 
+       <iframe width="300" height="200" src="https://www.youtube.com/embed/kcuZuR70Lpc" 
        title="YouTube video player" frameborder="0"
        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; 
        picture-in-picture; web-share" allowfullscreen></iframe>
