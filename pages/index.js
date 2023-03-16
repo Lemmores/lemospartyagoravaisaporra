534fd852cfba604155c6603f545f8d1f2874c2d7
Marcelo Lemos <108501838+Lemmores@users.noreply.github.com>
2023-03-15 23:14:53 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Página principal</title>
+        <title>LemosParty</title>
         <meta name="keywords" content="Roupas, Calçados, Bonés"></meta>
         <meta
           name="description"
